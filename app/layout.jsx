a5671822af0aa1e79ed2e8d9d4194cf51df439e7
EL OIDII ABDESSAMAD <aeloidii@gmail.com>
2024-06-28 21:05:53 +0100

    fix typo

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import "@styles/globals.css";
 
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Promptopia",
